4e86339c76beeca1bf77b87f328664a3d6826b19
Brandon Barnes <bbarnes1.stu.jsu.edu>
2019-02-17 15:41:25 -0600

    Intial Commit and Also finished up the getJSONData

---
+++ src/databasetojson_parser/DatabaseToJSON_Parser.java
@@ -0,0 +1,163 @@
+package databasetojson_parser;
+
+import java.sql.*;
+import org.json.simple.*;
+/**
+ *
+ * @author Brandon Barnes
+ */
+public class DatabaseToJSON_Parser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JSONArray results = getJSONData();
+        System.out.println(results);
+    }
+    
+    public static JSONArray getJSONData(){
+        JSONArray results = new JSONArray();
+        Connection conn = null;
+        PreparedStatement pstSelect = null, pstUpdate = null;
+        ResultSet resultset = null;
+        ResultSetMetaData metadata = null;
+        
+        String query, key, value;
+        String newFirstName = "Alfred", newLastName = "Neuman";
+        
+        boolean hasresults;
+        int resultCount, columnCount, updateCount = 0;
+        
+        try {
+            
+            /* Identify the Server */
+            
+            String server = ("jdbc:mysql://localhost/db_test");
+            String username = "root";
+            String password = "CS488";
+            System.out.println("Connecting to " + server + "...");
+            
+            /* Load the MySQL JDBC Driver */
+            
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            
+            /* Open Connection */
+
+            conn = DriverManager.getConnection(server, username, password);
+
+            /* Test Connection */
+            
+            if (conn.isValid(0)) {
+                
+                /* Connection Open! */
+                
+                System.out.println("Connected Successfully!");
+                
+                
+                /* Prepare Select Query */
+                
+                query = "SELECT * FROM people";
+                pstSelect = conn.prepareStatement(query);
+                
+                /* Execute Select Query */
+                
+                System.out.println("Submitting Query ...");
+                
+                hasresults = pstSelect.execute();                
+                
+                /* Get Results */
+                
+                System.out.println("Getting Results ...");
+                
+                while ( hasresults || pstSelect.getUpdateCount() != -1 ) {
+
+                    if ( hasresults ) {
+                        
+                        /* Get ResultSet Metadata */
+                        
+                        resultset = pstSelect.getResultSet();
+                        metadata = resultset.getMetaData();
+                        columnCount = metadata.getColumnCount();
+                        
+                        /* Get Column Names; Print as Table Header */
+                        
+                        JSONArray colHeaders = new JSONArray();
+                        
+                        for (int i = 2; i <= columnCount; i++) {
+
+                            colHeaders.add(metadata.getColumnLabel(i));
+
+                        }
+                        
+                        /* Get Data; Print as Table Rows */
+                        
+                        while(resultset.next()) {
+                            
+                            /* Begin Next ResultSet Row */
+
+                            System.out.println();
+                            
+                            JSONObject currentSet = new JSONObject();
+                            
+                            /* Loop Through ResultSet Columns; Print Values */
+
+                            for (int i = 0; i <= columnCount -2; i++) {
+                                
+                                for(int j = 1; j <= columnCount; j++){
+                                    
+                                    String colName = (String) colHeaders.get(i);
+                                    
+                                    currentSet.put(colHeaders.get(i), resultset.getString(colName));
+                                    
+                                }
+                            }
+                            results.add(currentSet);
+                        }
+                        
+                    }
+
+                    else {
+
+                        resultCount = pstSelect.getUpdateCount();  
+
+                        if ( resultCount == -1 ) {
+                            break;
+                        }
+
+                    }
+                    
+                    /* Check for More Data */
+
+                    hasresults = pstSelect.getMoreResults();
+
+                }
+                
+            }
+            
+            System.out.println();
+            
+            /* Close Database Connection */
+            
+            conn.close();
+            
+        }
+        
+        catch (Exception e) {
+            System.err.println(e.toString());
+        }
+        
+        /* Close Other Database Objects */
+        
+        finally {
+            
+            if (resultset != null) { try { resultset.close(); resultset = null; } catch (Exception e) {} }
+            
+            if (pstSelect != null) { try { pstSelect.close(); pstSelect = null; } catch (Exception e) {} }
+            
+            if (pstUpdate != null) { try { pstUpdate.close(); pstUpdate = null; } catch (Exception e) {} }
+            
+        }
+        return results;
+    }
+}
